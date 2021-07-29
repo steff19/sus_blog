d5971fd9e50974c3afad39a298f0b02e85dfc7ee
Stefan McDonald <stefan19mcdonald@hotmail.com>
2021-07-29 16:20:22 -0400

    Added edit,delete and show routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,42 @@ class PostsController < ApplicationController
         @posts = Post.all
     end
 
+    def new
+        @post = Post.new
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+
+    end
+
+    def create
+        @post = Post.new(params.require(:post).permit(:title, :description))
+        if @post.save
+            flash[:notice] = "Post Was Successfully Saved"
+        redirect_to @post
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @post = Post.find(params[:id])
+       if @post.update(params.require(:post).permit(:title, :description))
+            flash[:notice] = "Edit Successful"
+        redirect_to @post
+       else
+        render 'edit'
+       end
+
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path
+
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root'pages#home'
   get 'about' , to:'pages#about'
-  resources :posts, only: [:show, :index]
+  resources :posts
 end
